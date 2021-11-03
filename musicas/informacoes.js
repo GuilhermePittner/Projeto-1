1e3ea1d7f2e4a9a16d1f24e83fae4bca452658b4
GuilhermePittner <guilhermepittner123@gmail.com>
2021-11-03 20:33:35 -0300

    primeiro-commit

---
+++ musicas/informacoes.js
@@ -0,0 +1,184 @@
+let songs01 = [{
+    "autor": "blink182",
+    "nome": "The first time",
+    "link": "https://www.youtube.com/watch?v=ayXPRkTU3ZM"
+}]
+
+let songs02 = [{
+    "autor": "XXXTENTACION",
+    "nome": "Everybody Dies In Their Nightmares",
+    "link": "https://www.youtube.com/watch?v=7JGDWKJfgxQ"
+},
+{
+    "autor": "Linkin Park",
+    "nome": "One More Light",
+    "link": "https://www.youtube.com/watch?v=3kaUvGSLMew" 
+},
+{
+    "autor": "Simple Plan",
+    "nome": "Perfect",
+    "link": "https://www.youtube.com/watch?v=RUi54JTgL5s"
+}]
+
+let songs03 = [{
+    "autor": "Oasis",
+    "nome": "Wonderwall",
+    "link": "https://www.youtube.com/watch?v=ZrOKjDZOtkA"
+},
+{
+    "autor": "Edith Piaf",
+    "nome": "La vie en rose",
+    "link": "https://www.youtube.com/watch?v=OCVmZd1d4WI"
+},
+{
+    "autor": "Wünsche",
+    "nome": "Flower",
+    "link": "https://www.youtube.com/watch?v=Hs2r3IRSZg0"
+}]
+
+let songs04 = [{
+    "autor": "Detonautas",
+    "nome": "Quando o Sol Se For",
+    "link": "https://www.youtube.com/watch?v=9s3o85FqiVA"
+},
+{
+    "autor": "blink182",
+    "nome": "Darkside",
+    "link": "https://www.youtube.com/watch?v=4cbSPNZryzo"
+}]
+
+let songs05 = [{
+    "autor": "Bring me the horizon",
+    "nome": "Can you feel my heart",
+    "link": "https://www.youtube.com/watch?v=6AVRCQBc59w"
+},
+{
+    "autor": "blink182",
+    "nome": "I miss You",
+    "link": "https://www.youtube.com/watch?v=wNHGT-neTes"
+},
+{
+    "autor": "blink182",
+    "nome": "Remember to forget me",
+    "link": "https://www.youtube.com/watch?v=04cN6WZYxwQ"
+}]
+
+let songs06 = [{
+    "autor": "Charlie Brown Jr.",
+    "nome": "Como tudo deve ser",
+    "link": "https://www.youtube.com/watch?v=k7pr4VTk5cQ"
+},
+{
+    "autor": "Angels & Airwaves",
+    "nome": "Como tudo deve ser",
+    "link": "https://www.youtube.com/watch?v=k7pr4VTk5cQ"  
+}]
+
+let songs07 = [{
+    "autor": "blink182",
+    "nome": "Always",
+    "link": "https://www.youtube.com/watch?v=CvtJVku_mJw"
+},
+{
+    "autor": "Zedd",
+    "nome": "Clarity",
+    "link": "https://www.youtube.com/watch?v=IxxstCcJlsc"
+},
+{
+    "autor": "blink182",
+    "nome": "First Date",
+    "link": "https://www.youtube.com/watch?v=-3mTma5zL2Y"
+},
+{
+    "autor": "Calvin Harris",
+    "nome": "I need your love",
+    "link": "https://www.youtube.com/watch?v=AtKZKl7Bgu0"
+}]
+
+let songs08 = [{
+    "autor": "blink182",
+    "nome": "Happy Days",
+    "link": "https://www.youtube.com/watch?v=ahgkSf8qCd4"
+},
+{
+    "autor": "blink182",
+    "nome": "Heaven",
+    "link": "https://www.youtube.com/watch?v=NY0fKN0Q5H8"
+}]
+
+let songs09 = [{
+    "autor": "kupla",
+    "nome": "Valentine",
+    "link": "https://www.youtube.com/watch?v=UtmLRT47l1k"
+},
+{
+    "autor": "Bag Raiders",
+    "nome": "Shooting Stars",
+    "link": "https://www.youtube.com/watch?v=feA64wXhbjo"
+}]
+
+let songs10 = [{
+    "autor": "Three Days Grace",
+    "nome": "Lost in You",
+    "link": "https://www.youtube.com/watch?v=yaYJ68Akj2M"
+},
+{
+    "autor": "Three Days Grace",
+    "nome": "I hate everything about You",
+    "link": "https://www.youtube.com/watch?v=d8ekz_CSBVg"
+}]
+
+let songs11 = [{
+    "autor": "Red Hot Chili Peppers",
+    "nome": "Scar Tissue",
+    "link": "https://www.youtube.com/watch?v=mzJj5-lubeM"
+},
+{
+    "autor": "Shiloh",
+    "nome": "My Girl",
+    "link": "https://www.youtube.com/watch?v=0g_nwj7sYok"  
+},
+{
+    "autor": "Shiloh",
+    "nome": "My Girl",
+    "link": "https://www.youtube.com/watch?v=0g_nwj7sYok"
+},
+{
+    "autor": "Tra$h",
+    "nome": "Journey",
+    "link": "https://www.youtube.com/watch?v=ubsiPgZMRPw"
+}
+]
+
+let songs12 = [{
+    "autor": "John Newman",
+    "nome": "Love Me Again",
+    "link": "https://www.youtube.com/watch?v=CfihYWRWRTQ"
+},
+{
+    "autor": "Unknow",
+    "nome": "lofi - i love you baby",
+    "link": "https://www.youtube.com/watch?v=zk2ewxRmxog"
+}]
+
+let songs13 = [{
+    "autor": "blink182",
+    "nome": "Always",
+    "link": "https://www.youtube.com/watch?v=CvtJVku_mJw"
+},
+{
+    "autor": "Martin Garrix",
+    "nome": "In The Name Of Love",
+    "link": "https://www.youtube.com/watch?v=RnBT9uUYb1w"
+}]
+
+let songs14 = [{
+    "autor": "Avril Lavigne",
+    "nome": "Complicated",
+    "link": "https://www.youtube.com/watch?v=5NPBIwQyPWE"
+},
+{
+    "autor": "Milky Chance",
+    "nome": "Stolen Dance",
+    "link": "https://www.youtube.com/watch?v=A60S_Fkd2XY"
+}]
---
+++ script.js
@@ -0,0 +1,307 @@
+//variáveis únicas//
+var valorFeliz = 0; var valorTriste = 0; var valorApaixonado = 0; var valorInspirado = 0; var valorHearthbroken = 0; var valorChilling = 0
+//variáveis únicas//
+
+//variáveis duplas//
+var felizApaixonado = 0; var felizInspirado = 0; var felizChilling = 0; var tristePartido = 0; var tristeChilling = 0; var apaixonadoInspirado = 0;var apaixonadoChilling = 0;var hearthbrokenChilling = 0;
+//variáveis duplas//
+
+//variável de erro//
+var excesso = 0
+//variável de erro//
+
+//variável de sorteio//
+var sorteio
+//variável de sorteio//
+
+//arrumar a tela para o usuário re-inserir os números
+function limpando(){
+    limpar()
+    descolorir()
+}
+function limpar(){
+    valorFeliz = 0; valorTriste = 0;valorApaixonado = 0;valorInspirado = 0;valorHearthbroken = 0;valorChilling = 0;felizApaixonado = 0; felizInspirado = 0; felizChilling = 0; tristePartido = 0;  tristeChilling = 0; apaixonadoInspirado = 0;apaixonadoChilling = 0;hearthbrokenChilling = 0;excesso = 0
+}
+function descolorir(){
+    happyMood.classList.remove("fixar-feliz")
+    sadMood.classList.remove("fixar-triste")
+    loveMood.classList.remove("fixar-apaixonado")
+    inspiredMood.classList.remove("fixar-inspirado")
+    hearthbrokenMood.classList.remove("fixar-partido")
+    chillingMood.classList.remove("fixar-relaxando")
+}
+function again(){
+    limpar()
+    descolorir()
+    location.reload();
+}
+//arrumar a tela para o usuário re-inserir os números
+
+
+
+//FUNÇÕES DOS BOTÕES//
+//FUNÇÕES DOS BOTÕES//
+//FUNÇÕES DOS BOTÕES//
+
+function feliz(){
+    //seção de atribuição de valores
+    valorFeliz++
+    felizApaixonado++
+    felizInspirado++
+    felizChilling++
+    excesso++
+   
+    //seção para preencher a cor do botão
+    let happyMood=document.querySelector("#happyMood")
+    happyMood.classList.add("fixar-feliz")
+}
+
+function triste(){
+    //seção de atribuição de valores
+    valorTriste++  
+    tristePartido++ 
+    tristeChilling++
+    excesso++
+    
+    //seção para preencher a cor do botão
+    let sadMood=document.querySelector("#sadMood")
+    sadMood.classList.add("fixar-triste")
+}
+
+function apaixonado(){
+    //seção de atribuição de valores
+    valorApaixonado++
+    felizApaixonado++
+    apaixonadoInspirado++
+    apaixonadoChilling++
+    excesso++
+      
+    //seção para preencher a cor do botão
+    let loveMood=document.querySelector("#loveMood")
+    loveMood.classList.add("fixar-apaixonado")
+}
+
+function inspirado(){
+    //seção de atribuição de valores
+    valorInspirado++ 
+    felizInspirado++
+    apaixonadoInspirado++
+    excesso++
+
+    //seção para preencher a cor do botão
+    let inspiredMood=document.querySelector("#inspiredMood")
+    inspiredMood.classList.add("fixar-inspirado")
+}
+
+function hearthbroken(){
+    //seção de atribuição de valores
+    valorHearthbroken++
+    tristePartido++
+    hearthbrokenChilling++
+    excesso++
+
+    //seção para preencher a cor do botão
+    let hearthbrokenMood=document.querySelector("#hearthbrokenMood")
+    hearthbrokenMood.classList.add("fixar-partido")
+}
+
+function chilling(){
+    //seção de atribuição de valores
+    valorChilling++
+    felizChilling++
+    tristeChilling++
+    apaixonadoChilling++
+    hearthbrokenChilling++
+    excesso++
+
+    //seção para preencher a cor do botão
+    let chillingMood=document.querySelector("#chillingMood")
+    chillingMood.classList.add("fixar-relaxando")
+}
+
+function checar(){
+    //checagem para ver se há um excesso de escolhas
+    if (excesso>=3){
+        limpar()
+        descolorir()
+    }
+    if (valorFeliz>=2 || valorInspirado>=2 || valorApaixonado>=2 || valorTriste>=2 || valorChilling>=2 || valorHearthbroken>=2){
+        limpar()
+        descolorir()
+    }
+    //checagem para ver se há um excesso de escolhas
+
+    if (felizApaixonado==2){
+        valorFeliz=0
+        valorApaixonado=0
+
+        sorteio = Math.floor(Math.random()*songs07.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs07[sorteio].link}" target="_blank">${songs07[sorteio].nome}</a> by: ${songs07[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+        
+    } else if (felizInspirado==2){
+        valorFeliz=0
+        valorInspirado=0
+        
+        sorteio = Math.floor(Math.random()*songs08.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs08[sorteio].link}" target="_blank">${songs08[sorteio].nome}</a> by: ${songs08[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (felizChilling==2){
+        console.log(felizChilling+ "felizChilling")
+        valorFeliz=0
+        valorChilling=0
+
+        sorteio = Math.floor(Math.random()*songs09.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs09[sorteio].link}" target="_blank">${songs09[sorteio].nome}</a> by: ${songs09[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (tristePartido==2){
+        console.log(tristePartido+ "tristePartido")
+        valorTriste=0
+        valorHearthbroken=0
+
+        sorteio = Math.floor(Math.random()*songs10.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs10[sorteio].link}" target="_blank">${songs10[sorteio].nome}</a> by: ${songs10[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (tristeChilling==2){
+        console.log(tristeChilling+ "tristeChilling")
+        valorTriste=0
+        valorChilling=0
+
+        sorteio = Math.floor(Math.random()*songs11.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs11[sorteio].link}" target="_blank">${songs11[sorteio].nome}</a> by: ${songs11[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (apaixonadoInspirado==2){
+        console.log(apaixonadoInspirado+ "apaixonadoInspirado")
+        valorApaixonado=0
+        valorInspirado=0
+
+        sorteio = Math.floor(Math.random()*songs12.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs12[sorteio].link}" target="_blank">${songs12[sorteio].nome}</a> by: ${songs12[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if(apaixonadoChilling==2){
+        console.log(apaixonadoChilling+ "apaixonadoChilling") 
+        valorApaixonado=0
+        valorChilling=0
+
+        sorteio = Math.floor(Math.random()*songs13.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs13[sorteio].link}" target="_blank">${songs13[sorteio].nome}</a> by: ${songs13[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (hearthbrokenChilling==2){
+        console.log(hearthbrokenChilling+ "hearthbrokenchilling")
+        valorHearthbroken=0
+        valorChilling=0
+
+        sorteio = Math.floor(Math.random()*songs14.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs14[sorteio].link}" target="_blank">${songs14[sorteio].nome}</a> by: ${songs14[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (excesso==2){
+        alert("Ops, there was an error! Did you choose your mood correctly?")
+        limpar()
+        descolorir()
+    } else if (valorFeliz==1){
+
+        sorteio = Math.floor(Math.random()*songs01.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs01[sorteio].link}" target="_blank">${songs01[sorteio].nome}</a> by: ${songs01[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+        
+    } else if (valorTriste==1){
+
+        sorteio = Math.floor(Math.random()*songs02.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs02[sorteio].link}" target="_blank">${songs02[sorteio].nome}</a> by: ${songs02[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+        
+    } else if (valorApaixonado==1){
+        
+        sorteio = Math.floor(Math.random()*songs03.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs03[sorteio].link}" target="_blank">${songs03[sorteio].nome}</a> by: ${songs03[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (valorInspirado==1){
+        
+        sorteio = Math.floor(Math.random()*songs04.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs04[sorteio].link}" target="_blank">${songs04[sorteio].nome}</a> by: ${songs04[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (valorHearthbroken==1){
+
+        sorteio = Math.floor(Math.random()*songs05.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs05[sorteio].link}" target="_blank">${songs05[sorteio].nome}</a> by: ${songs05[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else if (valorChilling==1){
+
+        sorteio = Math.floor(Math.random()*songs06.length)
+
+        var showMusic=document.querySelector("#showMusic")
+        showMusic.innerHTML=(`The best music for you in this moment is: <a href="${songs06[sorteio].link}" target="_blank">${songs06[sorteio].nome}</a> by: ${songs06[sorteio].autor}`)
+
+        var reiniciar=document.querySelector("#reiniciar")
+        reiniciar.innerHTML=(`<button class="btn reiniciar" id="reiniciarMood" onclick="again()">Again!</button>`)
+
+    } else {
+        alert("Ops, there was an error! Did you choose your mood correctly?")
+        limpar()
+        descolorir()
+    } 
+}
